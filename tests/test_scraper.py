ac8ebf3d22665df881360813af87df36654e53e4
ZhenShuo2021 <98386542+ZhenShuo2021@users.noreply.github.com>
2024-10-27 04:33:27 +0800

    chore: code cleanup

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -5,24 +5,26 @@ import time
 import pytest
 
 from v2dl.config import ConfigManager
-from v2dl.custom_logger import setup_logging
+from v2dl.logger import setup_logging
 from v2dl.v2dl import ScrapeManager
 from v2dl.web_bot import get_bot
 
 os.environ["V2PH_USERNAME"] = "naf02905@inohm.com"  # temp account for testing
 os.environ["V2PH_PASSWORD"] = "VFc8v/Mqny"  # temp account for testing
 
+TEST_URL = "https://www.v2ph.com/album/Weekly-Big-Comic-Spirits-2016-No22-23"
+# TEST_URL = "https://www.v2ph.com/album/amem784a.html"
+BOT = "drission"
+
 
 @pytest.fixture
 def setup_test_env():
     # test_download_dir = tmp_path / "test_download"
     # test_download_dir.mkdir()
-    test_url = "https://www.v2ph.com/album/Weekly-Big-Comic-Spirits-2016-No22-23"
-    # test_url = "https://www.v2ph.com/album/amem784a.html"
+    test_url = TEST_URL
+    bot_type = BOT
     dry_run = False
     terminate = True
-    original_join = os.path.join
-    bot_type = "drission"
 
     logger = logging.getLogger("test_logger")
     logger.setLevel(logging.INFO)
@@ -56,12 +58,7 @@ def test_download(setup_test_env):
             break
         time.sleep(1)
 
-    print("=============================================")
-    print(test_download_dir)
-    print(os.path.exists(test_download_dir))
-    print("=============================================")
     downloaded_files = os.path.join(test_download_dir, os.listdir(test_download_dir)[0])
-
     assert len(downloaded_files) > 0, "No success downloads"
 
 
