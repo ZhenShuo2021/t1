5f9b6866f5491ed096396c66109add1d282deffa
ZhenShuo2021 <98386542+ZhenShuo2021@users.noreply.github.com>
2024-11-12 21:53:34 +0800

    refactor: separate staticmethods to a individual PathTool class

--- v2dl/common/config.py
+++ v2dl/common/config.py
@@ -62,7 +62,46 @@ class Config:
     chrome: ChromeConfig
 
 
-class ConfigManager:
+class PathTool:
+    @staticmethod
+    def resolve_abs_path(path, base_dir):
+        """Resolve '~', add path with base_dir if input is not absolute path."""
+        path = os.path.expanduser(path)
+        return os.path.join(base_dir, path) if not os.path.isabs(path) else path
+
+    @staticmethod
+    def get_system_config_dir() -> Path:
+        """Return the config directory."""
+        if platform.system() == "Windows":
+            base = os.getenv("APPDATA", "")
+        else:
+            base = os.path.expanduser("~/.config")
+        return Path(base) / "v2dl"
+
+    @staticmethod
+    def get_default_download_dir() -> Path:
+        return Path.home() / "Downloads"
+
+    @staticmethod
+    def get_download_dir(download_dir: str) -> str:
+        sys_dl_dir = PathTool.get_default_download_dir()
+        result_dir = (
+            PathTool.resolve_abs_path(download_dir, sys_dl_dir) if download_dir else sys_dl_dir
+        )
+        result_dir = Path(result_dir)
+        result_dir.mkdir(parents=True, exist_ok=True)
+        return str(result_dir)
+
+    @staticmethod
+    def get_chrome_exec_path(config_data: dict) -> str:
+        current_os = platform.system()
+        exec_path = config_data["chrome"]["exec_path"].get(current_os)
+        if not exec_path:
+            raise ValueError(f"Unsupported OS: {current_os}")
+        return exec_path
+
+
+class ConfigManager(PathTool):
     """Load and process configs based on user platform.
 
     The DEFAULT_CONFIG is a nested dict, after processing, the ConfigManager.load() returns a
@@ -96,57 +135,24 @@ class ConfigManager:
 
         # Check file paths
         for key, path in self.config["paths"].items():
-            self.config["paths"][key] = self.resolve_path(path, system_config_dir)
+            self.config["paths"][key] = ConfigManager.resolve_abs_path(path, system_config_dir)
 
-        self.config["chrome"]["profile_path"] = self.resolve_path(
+        self.config["chrome"]["profile_path"] = ConfigManager.resolve_abs_path(
             self.config["chrome"]["profile_path"], system_config_dir
         )
 
         # Check download_dir path
         download_dir = self.config["download"].get("download_dir", "").strip()
-        self.config["download"]["download_dir"] = self._get_download_dir(download_dir)
+        self.config["download"]["download_dir"] = ConfigManager.get_download_dir(download_dir)
 
         return Config(
             download=DownloadConfig(**self.config["download"]),
             paths=PathConfig(**self.config["paths"]),
             chrome=ChromeConfig(
-                exec_path=ConfigManager._get_chrome_exec_path(self.config),
+                exec_path=ConfigManager.get_chrome_exec_path(self.config),
                 profile_path=self.config["chrome"]["profile_path"],
             ),
         )
-
-    def resolve_path(self, path, base_dir):
-        """Resolve '~', add path with base_dir if input is not absolute path."""
-        path = os.path.expanduser(path)
-        return os.path.join(base_dir, path) if not os.path.isabs(path) else path
-
-    @staticmethod
-    def get_system_config_dir() -> Path:
-        """Return the config directory."""
-        if platform.system() == "Windows":
-            base = os.getenv("APPDATA", "")
-        else:
-            base = os.path.expanduser("~/.config")
-        return Path(base) / "v2dl"
-
-    @staticmethod
-    def get_default_download_dir() -> Path:
-        return Path.home() / "Downloads"
-
-    def _get_download_dir(self, download_dir: str) -> str:
-        sys_dl_dir = ConfigManager.get_default_download_dir()
-        result_dir = self.resolve_path(download_dir, sys_dl_dir) if download_dir else sys_dl_dir
-        result_dir = Path(result_dir)
-        result_dir.mkdir(parents=True, exist_ok=True)
-        return str(result_dir)
-
-    @staticmethod
-    def _get_chrome_exec_path(config_data: dict) -> str:
-        current_os = platform.system()
-        exec_path = config_data["chrome"]["exec_path"].get(current_os)
-        if not exec_path:
-            raise ValueError(f"Unsupported OS: {current_os}")
-        return exec_path
 
     @staticmethod
     def _merge_config(base: dict[str, Any], custom: dict[str, Any]) -> dict:
